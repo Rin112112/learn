ccfe9975ab2403a572d7bcc8ec4d195a01913514
BOT <mannarov@yandex.ru>
2024-01-27 18:25:39 +0300

    FIRST

---
+++ js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+// let number = 7;
+// const rightBoarderWidth = 2;
+// console.log(number);
+// // number = 18;
+// // console.log(number);
+// // const result = confirm(`are you chlen&`);
+// // console.log(result);
+// // const answer = prompt(`Будешь?`, '');
+// // console.log(answer + 7);
+// const answers = {};
+// answers [1] = prompt (`Туя?`, ``);
+// answers [0] = prompt (`Туя?`, ``);
+// answers [2] = prompt (`Туя?`, ``);
+// console.log(answers);
+// let incr = 10,
+//     decr = 10;
+//     incr++;
+//     decr--;
+//     console.log(incr);
+//     console.log(decr);-->
+const number0fFilms = prompt (`Сколько фильмов вы уже помиотрели?`, ``);
+
+// const answer = [];
+// answer[0] = prompt (`последний фильм?`, ``);
+// answer[1] = prompt (`последний фильм?`, ``);
+// answer[2] = prompt (`оценка?`, ``);
+// answer[3] = prompt (`оценка?`, ``);2
+
+// const obj = {
+//     Count: number0fFilms,
+//     Movies: {answer},
+//     Actors: {},
+//     Genres: [],
+//     Privat: Boolean `false`
+// }
+// console.log(obj.Movies);
+// console.log(obj.Count);
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt(`Фильм?`, ``),
+b = prompt(`Оценка?`, ``),
+c = prompt(`Фильм?`, ``),
+d = prompt(`оценка`,``);
+personalMovieDB.count
+personalMovieDB.movies[a]
+personalMovieDB.movies[c]
+console.log(personalMovieDB);
