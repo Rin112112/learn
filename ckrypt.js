afbb53dfe578d5b1509f8cef2d19008403728802
BOT <mannarov@yandex.ru>
2024-05-02 15:46:33 +0300

    doble

---
+++ ckrypt.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// let number = 4.6;
+
+// console.log (-4/0);
+// console.log('string' * 9);
+// const persone = "Zhopa"
+// const bpol = true;
+// console.log(some);
+// const vrvr = {
+//     name: "fjhgjf",
+//     age: 42,
+//     isFuck: true
+// };
+
+// // console.log(vrvr.name);
+// console.log(vrvr ["age"]);
+
+// let arr = ["awdaw", "awdaa", 234, "sda", {}, []]
+// console.log(arr[2]);
+// alert ("kajedkja");
+//  const resu = confirm("asdasdas&")
+//  console.log(resu);
+// const ans = prompt("zhopa?", "2")
+// console.log(ans);
+
+const answers = [];
+
+answers [0] = prompt("ваше?");
+answers [1] = prompt("имя?");
+answers [2] = prompt("наше?");
+answers [3] = prompt("ихнее?");
+
+document.write(answers);
